a8b599aaba48b9ec8c72d1fc0df928c04ebf294f
Chris Cirelli <ccirelli2@gmail.com>
2019-12-23 13:33:32 -0500

    commit

--- circuit_digest_tutorial/getting_started.py
+++ circuit_digest_tutorial/getting_started.py
@@ -4,12 +4,51 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-
 # Documentation
 '''
 Tutorial:       https://circuitdigest.com/tutorial/getting-started-with-opencv-image-processing
 
 '''
 
+# Load Images 
+os.chdir('/home/ccirelli2/Desktop/repositories/OpenCV_tutorial/data')
+img_cyber = cv2.imread('cyber_image.jpg')
+
+
+
+# Grey Scaling Image ------------------------------------------------------
+def show_img(img):
+    cv2.imshow('Cyber Image', img)
+    cv2.waitKey()
+
+
+def image_grey(img):
+    "You also add 0 to imread('name', 0)"
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow(image_gray)
+
+def get_shape(img):
+    print('Image shape = {}'.format(img.shape))
+
+
+def get_color(img):
+    'cv2 stores colors in BGR, not RBG'
+    print(img[100,100]) # should return three colors for this pixel
+
+
+def split_color_img(img):
+    'split image into three planes of each color'
+    B, G, R = cv2.split(img)
+    print('Red', R)
+    print('Green' , G)
+
+
+def histogram_rep_of_img(img):
+    '''Create a histogram representation of image, freq of intensity'''
+    B, G, R = cv2.split(img)
+    plt.hist(list(B))
+    plt.show()
+
+histogram_rep_of_img(img_cyber)
 
 
---
+++ circuit_digest_tutorial/image_segmentation.py
@@ -0,0 +1,37 @@
+# Documentation ---------------------------------------------------------------
+'''
+https://circuitdigest.com/tutorial/image-manipulation-in-python-opencv-part2
+
+
+In this tutorial, we are going to see some more image manipulations using Python OpenCV. Here we will learn to apply the following function on an image using Python OpenCV:
+
+Bitwise Operations and Masking
+Convolution & Blurring
+Sharpening - Reversing the image blurs
+Thresholding (Binarization)
+Dilation, Erosion, Opening/Closing
+Edge detection and Image gradients
+Perspective & Affine Transform
+Live Sketch Application
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
